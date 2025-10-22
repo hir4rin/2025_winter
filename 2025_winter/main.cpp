47f34d65a8e08d4f8c0182119fc797172477075a
hir4rin <fko2547051@stu.o-hara.ac.jp>
2025-10-23 01:18:17 +0900

    up

---
+++ 2025_winter/main.cpp
@@ -0,0 +1,40 @@
+#include "DxLib.h"
+
+// プログラムは WinMain から始まります
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	if (DxLib_Init() == -1)		// ＤＸライブラリ初期化処理
+	{
+		return -1;			// エラーが起きたら直ちに終了
+	}
+	//ダブルバッファモード
+	SetDrawScreen(DX_SCREEN_BACK);
+
+	while (ProcessMessage() == 0)
+	{
+		LONGLONG time = GetNowHiPerformanceCount();
+		//画面のクリア
+		ClearDrawScreen();
+
+		//裏画面と表画面を入れ替える
+		ScreenFlip();
+
+		//fpsを60に固定
+		while (GetNowHiPerformanceCount() - time < 16667)
+		{
+
+		}
+
+		if (CheckHitKey(KEY_INPUT_ESCAPE))
+		{
+			break;
+		}
+	}
+
+
+
+
+	DxLib_End();				// ＤＸライブラリ使用の終了処理
+
+	return 0;				// ソフトの終了 
+}
