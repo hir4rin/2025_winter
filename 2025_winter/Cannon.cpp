5ad97b16cb00f15f19f60e81a355a7a2db2a52c8
hir4rin <fko2547051@stu.o-hara.ac.jp>
2026-01-21 00:45:03 +0900

    大砲を変えた

--- 2025_winter/Cannon.cpp
+++ 2025_winter/Cannon.cpp
@@ -19,6 +19,10 @@ namespace
 		constexpr int  kCannonFireDuration = 5;
 		constexpr float drawOffSetY = 50.0f;//描画オフセットY座標
 
+		//大砲
+		constexpr int  kCannonWidth = 500;
+		constexpr int kCannonHeight = 550;
+
 	constexpr float  kFrozenScale = 5.0f;
 	
 }
@@ -32,7 +36,7 @@ Cannon::Cannon():
 	m_animFrame(0),
 	m_pos(400.0f, 720.0f)
 {
-	 m_handle = LoadGraph("data/cannonSub.png");
+	 m_handle = LoadGraph("data/Cannon.png");
 	assert(m_handle != -1 && "Cannon画像の読み込みに失敗しました");
 	m_colRect.SetCenter(0.0f, 0.0f, 64.0f, 64.0f);
 	m_state = CannonState::Idle;
@@ -48,32 +52,32 @@ void Cannon::Init()
 void Cannon::Update()
 {
 	m_animFrame++;
-	switch (m_state)
-	{
-		case CannonState::Idle://待機
-		//待機状態の処理
-			break;
-		case CannonState::Transform://変形
-			//最後のコマで止める
-			if (charaIdx == kCannonTransFormNum - 1)
-			{
-				charaIdx = kCannonTransFormNum - 1;
-			}
-			else//アニメーション進行 
-				charaIdx = (m_animFrame / kCannonDuration) % kCannonTransFormNum;
-			
-			break;
-		case CannonState::Fire://発射
-			//最後のコマで止める
-			if (charaIdx == kCannonFireNum - 1)
-			{
-				charaIdx = kCannonFireNum - 1;
-			}
-			else//アニメーション進行
-				charaIdx = (m_animFrame / kCannonFireDuration) % kCannonFireNum;
-				
-				break;
-	}
+	//switch (m_state)
+	//{
+	//	case CannonState::Idle://待機
+	//	//待機状態の処理
+	//		break;
+	//	case CannonState::Transform://変形
+	//		//最後のコマで止める
+	//		if (charaIdx == kCannonTransFormNum - 1)
+	//		{
+	//			charaIdx = kCannonTransFormNum - 1;
+	//		}
+	//		else//アニメーション進行 
+	//			charaIdx = (m_animFrame / kCannonDuration) % kCannonTransFormNum;
+	//		
+	//		break;
+	//	case CannonState::Fire://発射
+	//		//最後のコマで止める
+	//		if (charaIdx == kCannonFireNum - 1)
+	//		{
+	//			charaIdx = kCannonFireNum - 1;
+	//		}
+	//		else//アニメーション進行
+	//			charaIdx = (m_animFrame / kCannonFireDuration) % kCannonFireNum;
+	//			
+	//			break;
+	//}
 	m_colRect.SetCenter(m_pos.x, m_pos.y, 64.0f*2, 64.0f * 2);
 
 }
@@ -85,29 +89,32 @@ void Cannon::Draw(Camera& camera)
 #ifdef _DEBUG
 	m_colRect.DrawCamera(camera.drawOffset.x, camera.drawOffset.y, GetColor(255, 255, 255), false);
 #endif
-	switch (m_state)
-	{
-		case CannonState::Idle:
-			DrawRectRotaGraph(m_pos.x + camera.drawOffset.x, m_pos.y + camera.drawOffset.y,
-		kCannonTransFormCutW * 0, kCannonTransFormCutH,//切り取り左上
-		kCannonTransFormCutW, kCannonTransFormSorcH,//切り取りの幅
-		kFrozenScale, 0.0f, m_handle, true, true);
-			break;
-		case CannonState::Transform:
-			DrawRectRotaGraph(m_pos.x + camera.drawOffset.x, m_pos.y + camera.drawOffset.y,
-		kCannonTransFormCutW * charaIdx, kCannonTransFormCutH,//切り取り左上
-		kCannonTransFormCutW, kCannonTransFormSorcH,//切り取りの幅
-		kFrozenScale, 0.0f, m_handle, true, true);
-			break;
-		case CannonState::Fire:
-			DrawRectRotaGraph(m_pos.x + camera.drawOffset.x, m_pos.y + camera.drawOffset.y+ drawOffSetY,
-		kCannonFireCutW * charaIdx, kCannonFireCutH,//切り取り左上
-		kCannonFireCutW, kCannonFireSorcH,//切り取りの幅
-		kFrozenScale, 0.0f, m_handle, true, false);
-			break;
-	}
+	//switch (m_state)
+	//{
+	//	case CannonState::Idle:
+	//		DrawRectRotaGraph(m_pos.x + camera.drawOffset.x, m_pos.y + camera.drawOffset.y,
+	//	kCannonTransFormCutW * 0, kCannonTransFormCutH,//切り取り左上
+	//	kCannonTransFormCutW, kCannonTransFormSorcH,//切り取りの幅
+	//	kFrozenScale, 0.0f, m_handle, true, true);
+	//		break;
+	//	case CannonState::Transform:
+	//		DrawRectRotaGraph(m_pos.x + camera.drawOffset.x, m_pos.y + camera.drawOffset.y,
+	//	kCannonTransFormCutW * charaIdx, kCannonTransFormCutH,//切り取り左上
+	//	kCannonTransFormCutW, kCannonTransFormSorcH,//切り取りの幅
+	//	kFrozenScale, 0.0f, m_handle, true, true);
+	//		break;
+	//	case CannonState::Fire:
+	//		DrawRectRotaGraph(m_pos.x + camera.drawOffset.x, m_pos.y + camera.drawOffset.y+ drawOffSetY,
+	//	kCannonFireCutW * charaIdx, kCannonFireCutH,//切り取り左上
+	//	kCannonFireCutW, kCannonFireSorcH,//切り取りの幅
+	//	kFrozenScale, 0.0f, m_handle, true, false);
+	//		break;
+	//}
 	
-
+	DrawRectRotaGraph(m_pos.x + camera.drawOffset.x, m_pos.y + camera.drawOffset.y + 50.0f,
+	kCannonTransFormCutW * 0, 0,//切り取り左上
+	kCannonWidth, kCannonHeight,//切り取りの幅
+	0.3f, 0.0f, m_handle, true, false);
 }
 void Cannon::Fire()
 {
